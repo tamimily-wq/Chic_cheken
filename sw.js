b172c9fd26f80d8262fd7c6def71853195b3f42b
tamimily-wq <tamimi.ly@gmail.com>
2026-07-12 18:09:24 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-
-const CACHE='accounting-v73-supabase';
-const ASSETS=['./','./FINAL-V73-SUPABASE-GUARANTEED-BACKUP.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./FINAL-V73-SUPABASE-GUARANTEED-BACKUP.html'))))});
