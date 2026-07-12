9fbe44601e63f7ea1899239d62968b73658fc0d9
tamimily-wq <tamimi.ly@gmail.com>
2026-07-12 23:51:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-// V78 - NO CACHE - يمنع مشكلة الدخول
-self.addEventListener('install', e=>{ self.skipWaiting(); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{ e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
+// V79 - KILL ALL CACHE - حل نهائي لمشكلة الدخول العالق
+self.addEventListener('install', e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e=>{
+  // لا تخزن شي - جيب من الشبكة مباشرة
+  e.respondWith(fetch(e.request, {cache:'no-store'}).catch(()=>fetch(e.request)));
+});
