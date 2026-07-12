99c0cd0ae298b3a4899ac8b4203482d9dd80fb4c
tamimily-wq <tamimi.ly@gmail.com>
2026-07-13 01:29:52 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// V80 - KILL CACHE FOREVER
-self.addEventListener('install', e=>{ self.skipWaiting(); e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x))))); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{ e.respondWith(fetch(e.request, {cache:'no-store'})); });
