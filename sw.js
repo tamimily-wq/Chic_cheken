81010c9d4217ec1402f8856139d59e69b258525a
tamimily-wq <tamimi.ly@gmail.com>
2026-07-13 01:46:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-// V80 FINAL - KILL ALL CACHE - يمنع تعليق الدخول نهائيا
-self.addEventListener('install', e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x))))
-    .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(fetch(e.request, {cache:'no-store'}).catch(()=>fetch(e.request)));
-});
+// V81 - NO CACHE - يمنع تعليق الدخول نهائيا - لا يخزن شي
+self.addEventListener('install', e=>{ self.skipWaiting(); e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e=>{ e.respondWith(fetch(e.request, {cache:'no-store', credentials:'omit'})); });
