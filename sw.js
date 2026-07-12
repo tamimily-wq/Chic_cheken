6cd9583925ae1ffc51f01947d12a86bbe32658b0
tamimily-wq <tamimi.ly@gmail.com>
2026-07-12 23:37:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,4 @@
-// V75 - NO CACHE - يقتل كاش V11 نهائيا
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', e => {
-  // لا تخزن شي - جيب ديما من الشبكة
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+// V78 - NO CACHE - يمنع مشكلة الدخول
+self.addEventListener('install', e=>{ self.skipWaiting(); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e=>{ e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
