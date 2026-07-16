4d17b6b08d5427f530b336d8613e546f0ddbfc15
tamimily-wq <tamimi.ly@gmail.com>
2026-07-16 10:52:57 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// V81 - NO CACHE - يمنع تعليق الدخول نهائيا - لا يخزن شي
-self.addEventListener('install', e=>{ self.skipWaiting(); e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{ e.respondWith(fetch(e.request, {cache:'no-store', credentials:'omit'})); });
